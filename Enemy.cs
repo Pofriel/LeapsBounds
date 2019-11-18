a015f90f093ff82c1bf8e18696838c9d143f20af
pofriel <ofrielp@gmail.com>
2019-11-17 21:24:30 -0500

    Start to add root scene

--- Enemy.cs
+++ Enemy.cs
@@ -9,25 +9,35 @@ public class Enemy : KinematicBody2D
     Vector2 Velocity = new Vector2();
     
     [Export]
-    float XSpeed = 25;
+    public float XSpeed = 25;
 
     [Export]
-    float WalkLimit = 10;
+    public float WalkLimit = 10;
     
     public override void _Ready()
     {
         Velocity = new Vector2(XSpeed, 0);
     }
 
+    [Export]
+    public bool Freeze = false;
+
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        TimeWalkin += delta;
-        if (TimeWalkin > WalkLimit) {
-            XSpeed *= -1;
-            TimeWalkin = 0;
+        if (!Freeze) {
+            TimeWalkin += delta;
+            if (TimeWalkin > WalkLimit) {
+                XSpeed *= -1;
+                TimeWalkin = 0;
+            }
+            Velocity = new Vector2(XSpeed, 0);
+            Velocity = MoveAndSlide(Velocity);
         }
-        Velocity = new Vector2(XSpeed, 0);
-        Velocity = MoveAndSlide(Velocity);
+        else {
+            Velocity = new Vector2(0, 0);
+            Velocity = MoveAndSlide(Velocity);
+        }
+        
     }
 }
