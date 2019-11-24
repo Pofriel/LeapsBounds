2d416b1b270bf3e135dd8927d726f088bda74357
pofriel <ofrielp@gmail.com>
2019-11-24 17:19:59 -0500

    Handle bomb creation on teleport

--- RootScene.cs
+++ RootScene.cs
@@ -4,18 +4,32 @@ using System;
 public class RootScene : Node2D
 {
    
+
+    
+    public PackedScene BombScene = GD.Load<PackedScene>(
+        "res://Bomb.tscn"
+    );
     public Enemy MyEnemy;
 
     public Player MyPlayer;
+
+    public Bomb PlayerBomb; 
     public override void _Ready()
     {
         MyEnemy = GetNode<Enemy>("Enemy");
         MyPlayer = GetNode<Player>("Player");
         MyPlayer.Connect("ProtonLeap", this, nameof(_OnProtonLeap));
+        MyPlayer.Connect("CreateBomb", this, nameof(_OnCreateBomb));
     }
 
     private void _OnProtonLeap(bool isActive) {
         MyEnemy.Freeze = isActive;
+    }
+
+    private void _OnCreateBomb(Vector2 position) {
+        PlayerBomb = BombScene.Instance() as Bomb;
+        AddChild(PlayerBomb);
+        PlayerBomb.GlobalPosition = position;
     }
 
 }
